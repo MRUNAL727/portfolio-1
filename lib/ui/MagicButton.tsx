537b894170061db9da60200fb3c2291715645370
MRUNAL727 <mrunal4271@gmail.com>
2025-07-02 12:42:05 +0530

    fixed build issue

--- lib/ui/MagicButton.tsx
+++ lib/ui/MagicButton.tsx
@@ -18,6 +18,7 @@ const MagicButton = ({
       <button
         className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] 
       focus:outline-non w-full md:w-60 md:mt-10"
+        onClick={handleClick}
       >
         <span
           className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] 
